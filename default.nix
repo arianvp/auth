e2acb87731a36ede5f5a9d890f3ba0b03613930e
Arian van Putten <arian.vanputten@gmail.com>
2022-08-10 16:08:54 +0200

    Without a default.nix

--- default.nix
+++
@@ -1,6 +0,0 @@
-{ buildGoModule }:
-buildGoModule {
-  name = "auth";
-  src = ./.;
-  vendorHash = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
-}
--- flake.nix
+++ flake.nix
@@ -7,7 +7,11 @@
 
       packages = { pkgs, selfPackages }: {
         default = selfPackages.auth;
-        auth = pkgs.callPackage ./. { };
+        auth = pkgs.buildGoModule {
+          name = "auth";
+          src = ./.;
+          vendorHash = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+        };
       };
     in
     {
