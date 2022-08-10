6f70f8f45e786477935604029ffe139ef104d471
Arian van Putten <arian.vanputten@gmail.com>
2022-08-10 16:02:53 +0200

    Add x86_64-linux support

--- flake.nix
+++ flake.nix
@@ -3,15 +3,23 @@
 
   outputs = { self, nixpkgs }:
     let
-      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+      pkgs = nixpkgs.legacyPackages;
+
+      packages = { pkgs, selfPackages }: {
+        default = selfPackages.auth;
+        auth = pkgs.callPackage ./. { };
+      };
     in
     {
-
-      packages.aarch64-darwin.default = self.packages.aarch64-darwin.auth;
-      packages.aarch64-darwin.auth = pkgs.buildGoModule {
-        name = "auth";
-        src = ./.;
-	vendorHash = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+      packages = {
+        aarch64-darwin = packages {
+          pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+          selfPackages = self.packages.aarch64-darwin;
+        };
+        x86_64-linux = packages {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          selfPackages = self.packages.x86_64-linux;
+        };
       };
     };
 }
--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "log"
 
 func main() {
-	log.Print("hello world")
+	log.Print("hello world!")
 }
