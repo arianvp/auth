b7974ac557e3ea56eece354a4a46294cc5b3251b
Arian van Putten <arian.vanputten@gmail.com>
2022-08-29 11:44:03 +0200

    stuff

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "log"
+import (
+	"database/sql"
+	"flag"
+	"log"
+	"net/http"
+)
+
+var (
+	addr     = flag.String("addr", "", "The address on which the server listens")
+	certFile = flag.String("cert-file", "", "")
+	keyFile  = flag.String("key-file", "", "")
+	dsn      = flag.String("dsn", "", "")
+)
+
+type Handler struct {
+	db *sql.DB
+	*http.ServeMux
+}
 
 func main() {
-	log.Print("hello world!")
+	mux := http.NewServeMux()
+	db, err := sql.Open("psql", *dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	server := http.Server{
+		Addr:    *addr,
+		Handler: &Handler{db, mux},
+	}
+	log.Fatal(server.ListenAndServeTLS(*certFile, *keyFile))
 }
