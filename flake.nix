831b03ef83302e9527bd81dc9bf14de9d266bd81
Arian van Putten <arian.vanputten@gmail.com>
2022-09-20 17:34:04 +0200

    vendor

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
         auth = pkgs.buildGoModule {
           name = "auth";
           src = ./.;
-          vendorHash = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+          vendorHash = null;
         };
       };
     in
