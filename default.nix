a4312de7fa11834428a2b94a78fb374db4d7b0e7
Arian van Putten <arian.vanputten@gmail.com>
2022-11-26 13:52:48 +0000

    test

---
+++ default.nix
@@ -0,0 +1,7 @@
+{ buildGoModule }:
+buildGoModule {
+  pname = "auth";
+  version = "0.0.0";
+  src = ./.;
+  vendorHash = null;
+}
--- flake.nix
+++ flake.nix
@@ -1,29 +1,8 @@
 {
   description = "auth";
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = nixpkgs.legacyPackages;
-
-      packages = { pkgs, selfPackages }: {
-        default = selfPackages.auth;
-        auth = pkgs.buildGoModule {
-          name = "auth";
-          src = ./.;
-          vendorHash = null;
-        };
-      };
-    in
-    {
-      packages = {
-        aarch64-darwin = packages {
-          pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-          selfPackages = self.packages.aarch64-darwin;
-        };
-        x86_64-linux = packages {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          selfPackages = self.packages.x86_64-linux;
-        };
-      };
-    };
+  outputs = { self, nixpkgs }: {
+    packages.aarch64-darwin.auth = nixpkgs.legacyPackages.aarch64-darwin.callPackage ./. { };
+    packages.aarch64-darwin.default = self.packages.aarch64-darwin.auth;
+  };
 }
